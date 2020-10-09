88c87388471ac14feab17b13fa39aa13104643c6
jagarlapudi <jdurgakiran.hyd@gmail.com>
2020-10-09 17:33:04 -0400

    Added message to show if no records found

--- src/components/character/CardGrid.js
+++ src/components/character/CardGrid.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 const CardGrid = ({ items, isLoading }) => {
   return isLoading ? (
     <div className="loader"></div>
-  ) : (
+  ) : items.length > 0 ? (
     <div className="container-grid">
       {items.map((character) => (
         <Card
@@ -17,6 +17,10 @@ const CardGrid = ({ items, isLoading }) => {
           key={character.char_id}
         />
       ))}
+    </div>
+  ) : (
+    <div className="center">
+      <h2 className="norecs">No Records Found</h2>
     </div>
   );
 };
--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import debounce from "lodash/debounce";
 const Search = ({ getQuery }) => {
   const [queryText, setQueryText] = useState("");
 
   const ChangeHandler = debounce((text) => {
-    setQueryText(text);
     getQuery(text);
+    setQueryText(text);
   }, 2000);
+
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <section>
       <form className="search-form">
         <input
+          ref={inputRef}
           type="text"
           className="form-control"
           placeholder="Search Characters..."
